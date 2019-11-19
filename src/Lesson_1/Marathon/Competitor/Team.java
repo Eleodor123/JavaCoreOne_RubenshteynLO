75c5a2baa18b44db9551da8d2ded1d4c3fe8ae0b
l.rubenshteyn <levik77794@mail.ru>
2019-11-19 15:57:11 +0300

    asd

---
+++ src/Lesson_1/Marathon/Competitor/Team.java
@@ -0,0 +1,31 @@
+package Lesson_1.Marathon.Competitor;
+
+public class Team {
+    private String title;
+    private Competitor[] members;
+
+    public Competitor[] getMembers() {
+        return members;
+    }
+
+    public Team(String title, Competitor...members) {
+        this.title = title;
+        this.members = members;
+    }
+
+    public void showWinners(Competitor...members) {
+        for(Competitor o: members) {
+            if(o.isOnDistance()) {
+                o.info();
+            }
+        }
+    }
+
+    public void showWinners() {
+        for(Competitor o: members) {
+            if(o.isOnDistance()) {
+                o.info();
+            }
+        }
+    }
+}
