e1bae9f920eee42b90455e8e6be67a2746e829d2
l.rubenshteyn <l.rubenshteyn@sirena-travel.ru>
2019-11-22 14:49:42 +0300

    Исправлена структура первого урока, добавлено решение для второго урока про Exceptions, рефакторинг

--- src/Lesson_2/Fruit.java
+++ src/Lesson_2/Fruit.java
@@ -4,11 +4,11 @@ public enum Fruit {
     ORANGE("Апельсин", 3), APPLE("Яблоко", 2), BANANA("Бабнан", 4), CHERRY("Вишня", 1);
 
     private String rus;
-    private int weigth;
+    private int weight;
 
-    Fruit(String rus, int weigth) {
+    Fruit(String rus, int weight) {
         this.rus = rus;
-        this.weigth = weigth;
+        this.weight = weight;
     }
 
     public String getRus() {
@@ -19,12 +19,12 @@ public enum Fruit {
         this.rus = rus;
     }
 
-    public int getWeigth() {
-        return weigth;
+    public int getWeight() {
+        return weight;
     }
 
-    public void setWeigth(int weigth) {
-        this.weigth = weigth;
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 }
 
@@ -37,10 +37,8 @@ class MainFruit {
         //System.out.println(Fruit.APPLE);
 
         for (Fruit o: Fruit.values()) {
-            System.out.println(o.getRus() + " " + o + " " + o.getWeigth());
+            System.out.println(o.getRus() + " " + o + " " + o.getWeight());
         }
        // System.out.println(Fruit.BANANA.ordinal());
-
-
     }
 }
